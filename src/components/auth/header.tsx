24c6a285a6832ad904d1e1d4347baa36fe190e0f
abdizamedmo <abdizamedmo@gmail.com>
2023-10-24 11:37:05 +0300

    Auth header for using re-usable layout added

---
+++ src/components/auth/header.tsx
@@ -0,0 +1,35 @@
+import { BadgeRounded } from "@mui/icons-material";
+import { Box, IconButton, Typography } from "@mui/joy";
+import { ColorSchemeToggle } from "components";
+
+export function AuthHeader() {
+  return (
+    <Box
+      component="header"
+      sx={{
+        py: 3,
+        display: "flex",
+        alignItems: "left",
+        width: 400,
+        maxWidth: "100%",
+        mx: "auto",
+
+        justifyContent: "space-between",
+      }}
+    >
+      <Box
+        sx={{
+          gap: 2,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <IconButton variant="soft" color="primary" size="sm">
+          <BadgeRounded />
+        </IconButton>
+        <Typography level="title-lg">Next Wave</Typography>
+      </Box>
+      <ColorSchemeToggle />
+    </Box>
+  );
+}
