9d32054c2e095fc92c324d381052e0f248ff5a07
abdizamedmo <abdizamedmo@gmail.com>
2023-10-24 22:27:49 +0300

    Guards added (guest guard and auth guard)

--- src/guards/guest-guard.tsx
+++ src/guards/guest-guard.tsx
@@ -1,24 +1,19 @@
 import { useStores } from "models/helpers";
-import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { PATHS } from "router/paths";
 import { isEmpty, isBoolean, isNull } from "lodash";
 import { observer } from "mobx-react-lite";
 
-export const AuthGuard = observer(function AuthGuard() {
+export const GuestGuard = observer(function GuestGuard() {
   const {
     authStore: {
       isAuthenticated,
       tokens: { accessToken },
     },
   } = useStores();
-  const location = useLocation();
 
   const isAuth =
     !isNull(accessToken) && !isEmpty(accessToken) && isBoolean(isAuthenticated);
 
-  return isAuth ? (
-    <Outlet />
-  ) : (
-    <Navigate to={`${PATHS.auth.login}?next=${location.pathname}`} replace />
-  );
+  return !isAuth ? <Outlet /> : <Navigate to={PATHS.dashboard.root} replace />;
 });
