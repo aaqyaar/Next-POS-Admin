5d08a043ae16ec9bad7aa065711ca416f573c0d5
abdizamedmo <abdizamedmo@gmail.com>
2023-10-24 11:40:40 +0300

    Dashboard page is added for all navs and routes

---
+++ src/layouts/nav-config.tsx
@@ -0,0 +1,64 @@
+import {
+  DashboardRounded,
+  ShoppingCartRounded,
+  GroupRounded,
+  PersonRounded,
+  ReadMoreRounded,
+  ProductionQuantityLimitsRounded,
+} from "@mui/icons-material";
+import { FC } from "react";
+import { PATHS } from "router/paths";
+
+interface NavConfig {
+  path: string;
+  name: string;
+  icon?: FC;
+  children?: NavConfig[];
+}
+
+export const navConfig: NavConfig[] = [
+  {
+    path: PATHS.dashboard.root,
+    name: "Dashboard",
+    icon: DashboardRounded,
+  },
+  {
+    path: PATHS.dashboard.orders.root,
+    name: "Orders",
+    icon: ShoppingCartRounded,
+  },
+  {
+    path: PATHS.dashboard.customers.root,
+    name: "Customers",
+    icon: GroupRounded,
+  },
+  {
+    path: PATHS.dashboard.products.root,
+    name: "Products",
+    icon: ProductionQuantityLimitsRounded,
+  },
+  {
+    path: PATHS.dashboard.reports.root,
+    name: "Reports",
+    icon: ReadMoreRounded,
+  },
+  {
+    path: PATHS.dashboard.users.root,
+    name: "Users",
+    icon: PersonRounded,
+    children: [
+      {
+        path: PATHS.dashboard.users.profile,
+        name: "My profile",
+      },
+      {
+        path: PATHS.dashboard.users.add,
+        name: "Create a new user",
+      },
+      {
+        path: PATHS.dashboard.users.roles.root,
+        name: "Roles & permission",
+      },
+    ],
+  },
+];
